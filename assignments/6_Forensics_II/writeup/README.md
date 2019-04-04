b99b27642226631ca04a2d22aae5a77b882f3728
Christopher Lai <clai88@gmail.com>
2019-04-03 23:17:20 -0400

    did part 2

--- assignments/6_Forensics_II/writeup/README.md
+++ assignments/6_Forensics_II/writeup/README.md
@@ -26,7 +26,7 @@ Digital acknowledgement: Christopher Lai
 
 4. What port are they using to steal files on the server?
 
-- 20
+- 21
 
 5. Which file did they steal? What kind of file is it? Provide all metadata on the file. Specifically,
 
@@ -49,9 +49,47 @@ Digital acknowledgement: Christopher Lai
 - greetz.fpff
 
 7. What is a countermeasure to prevent this kind of intrusion from happening again? Note: disabling the vulnerable service is *not* an option.
-- allowing only authorized users from certain ip addresses to connect to the server
+- allowing only authorized users from certain ip addresses to connect to the server.  Also blacklisting any ips they catch performing a port scan
 
 ### Part 2 (55 Pts)
+
+2. Parse `greetz.fpff`, and report the following information:
+    1. When was `greetz.fpff` generated?
+    - ```2019-03-27 00:15:05```
+    2. Who authored `greetz.fpff`?
+    - ```f1inch```
+    4. List each section, giving us the data in it *and* its type.
+    ```
+        ------- HEADER -------
+    MAGIC: 0x8badf00d
+    VERSION: 1
+    TIMESTAMP: 1553660105 (2019-03-27 00:15:05)
+    AUTHOR: fl1nch
+    SECTION: 5
+    -------  BODY  -------
+    Section Type: 1
+    Section Length: 24
+    ASCII: Hey you, keep looking :)
+    Section Type: 6
+    Section Length: 16
+    Coordinates: (52.336035, 4.880673)
+    Section Type: 8
+    Section Length: 202776
+    PNG saved to embedded_png.png
+    Section Type: 1
+    Section Length: 44
+    ASCII: }R983CSMC_perg_tndid_u0y_yllufep0h{-R983CSMC
+    Section Type: 1
+    Section Length: 80
+    ASCII: CMSC389R-{hey_h3y_y0U_you_I_dont_like_your_base64_encoding}
+    ```
+    5. Report *at least* one flag hidden in `greetz.fpff`. Any other flag found will count as bonus points towards the *competition* portion of the syllabus.
+
+    - ```CMSC389R-{h0pefully_y0u_didnt_grep_CMSC389R}```
+
+    - ```CMSC389R-{hey_h3y_y0U_you_I_dont_like_your_base64_encoding}```
+
+    - ```CMSC389R-{w3lc0me_b@ck_fr0m_spr1ng_br3ak}```
 
 
 
--- assignments/6_Forensics_II/writeup/parse.py
+++ assignments/6_Forensics_II/writeup/parse.py
@@ -3,6 +3,7 @@
 import sys
 from struct import pack, unpack
 import time
+import base64
 
 
 # You can use this method to exit on failure conditions.
@@ -51,4 +52,87 @@ print("SECTION: {}".format(section))
 # the rest of the header and the actual FPFF body. Good luck!
 
 print("-------  BODY  -------")
+
+offset = 24
+for i in range(section):
+    stype, slen = unpack("<LL", data[offset:offset + 8])
+
+    print("Section Type: {}".format(stype))
+    print("Section Length: {}".format(slen))
+
+    offset += 8
+
+    # SECTION_ASCII (0x1)
+    if stype == 1:
+        (message,) = unpack("<{}s".format(slen), data[offset:offset + slen])
+
+        if message[-1] == "=":
+            message = base64.b64decode(message)
+            print("ASCII: {}".format(message))
+        else:
+            print("ASCII: {}".format(message))
+
+    # SECTION_UTF8 (0x2) -- UTF-8-encoded text.
+    if stype == 2:
+        (message,) = unpack("<{}s".format(slen), data[offset:offset + slen])
+        print("UTF-8: {}".format(message))
+
+    # SECTION_WORDS (0x3) -- Array of words.
+    if stype == 3:
+        (message,) = unpack("<{}s".format(slen/4), data[offset:offset + slen/4])
+        print("WORDS: {}".format(message))
+
+    # SECTION_DWORDS (0x4) -- Array of dwords.
+    if stype == 4:
+        for i in range(0, slen, 8):
+            (message, ) = unpack("<8s", data[offset + i, offset + i + 8])
+            print("DWORDS: {}".format(message))
+
+    # SECTION_DOUBLES (0x5) -- Array of doubles.
+    if stype == 5:
+         for i in range(0, slen, 8):
+            (message, ) = unpack("<8s", data[offset + i, offset + i + 8])
+            print("DOUBLES: {}".format(message))
+
+    # SECTION_COORD (0x6) -- (Latitude, longitude) tuple of doubles.
+    if stype == 6:
+        if slen == 16:
+            (latitude, longitude) = unpack("<dd", data[offset:offset + slen])
+            print("Coordinates: ({}, {})".format(latitude, longitude))
+        else:
+            print("Wrong length, expected 16, got {}".format(slen))
+
+    # SECTION_REFERENCE (0x7) -- The index of another section.
+    if stype == 7:
+        (message,) = unpack("<d", data[offset:offset+slen])
+        print("REFERENCE: {}".format(message))
+
+    # SECTION_PNG (0x8) -- Embedded PNG image.
+    if stype == 8:
+        header = b"\x89\x50\x4E\x47\x0d\x0a\x1a\x0a"
+        with open("embedded_png.png", "wb") as f:
+            f.write(header)
+            f.write(data[offset:offset + slen])
+
+        print("PNG saved to embedded_png.png")
+
+    # SECTION_GIF87 (0x9) -- Embedded GIF87.
+    if stype == 9:
+        header = b"\x47\x49\x46\x38\x37\x61"
+        with open("embedded_gif87.png", "wb") as f:
+            f.write(header)
+            f.write(data[offset:offset + slen])
+        
+        print("GIF87 saved to embedded_gif87.png")
+
+    # SECTION_GIF89 (0xA) -- Embedded GIF89.
+    if stype == 10:
+        header = b"\x47\x49\x46\x38\x39\x61"
+        with open("embedded_gif89.png", "wb") as f:
+            f.write(header)
+            f.write(data[offset:offset + slen])
+
+        print("GIF89 saved to embedded_gif89.png")
+
+    offset += slen
 

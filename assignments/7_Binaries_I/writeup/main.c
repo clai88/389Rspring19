2800eabf76c28a90f4dfadd013568f5511d7e3ef
Christopher Lai <clai88@gmail.com>
2019-04-09 22:26:25 -0400

    did hw 7

--- assignments/7_Binaries_I/writeup/main.c
+++ assignments/7_Binaries_I/writeup/main.c
@@ -1,11 +1,32 @@
 /*
- * Name: *PUT YOUR NAME HERE*
- * Section: *PUT YOUR SECTION NUMBER HERE*
+ * Name: Christopher Lai
+ * Section: 0101
  *
  * I pledge on my honor that I have not given or received any unauthorized
  * assistance on this assignment or examination.
  *
- * Digital acknowledgement: *PUT YOUR NAME HERE*
+ * Digital acknowledgement: Christopher Lai
  */
 
 /* your code goes here */
+
+#include <stdio.h>
+
+int main(){
+    int a = 0x1ceb00da;
+    int b = 0xfeedface;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    b = a ^ b;
+
+    a = a ^ b;
+
+    b = a ^ b;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
